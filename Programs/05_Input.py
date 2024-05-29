7d8743a23e005d780150d1b33b1b082bb23a6365
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-05-29 22:40:27 +0530

    05 - Program is added.

---
+++ Programs/05_Input.py
@@ -0,0 +1,11 @@
+"""
+Taking inputs form user and print it.
+Author: Mehdiali
+Date: 29 / May / 2024 - 01:10 PM
+"""
+
+name = input("Enter your name = ")
+age = int(input("Enter your age = "))
+marks = float(input("Enter your marks = "))
+
+print("\nYour name is", name, "and you are", age, "years old.\nYou got", marks, "marks.")
