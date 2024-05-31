6e81709df4ffc501105fcbc45881aad77c9d1270
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-05-31 11:05:37 +0530

    07 - Practice Questions program is added.

---
+++ Programs/07_Practice_Question.py
@@ -0,0 +1,27 @@
+"""
+Practice Questions
+Author: Mehdiali
+Date: 29 / May / 2024 - 09:54 PM
+"""
+
+num1 = int(input("Enter the 1st number = "))
+num2 = int(input("Enter the 2nd number = "))
+sum = num1 + num2
+print("Sum =", sum)
+
+print("\nArea of square:")
+side = float(input("Enter the side of square = "))
+area = side**2
+print("The area =", area)
+
+print("\nAverage of two float number:")
+num1 = float(input("Enter the 1st number = "))
+num2 = float(input("Enter the 2nd number = "))
+average = (num1 + num2) / 2
+print("Average =", average)
+
+print("\nGreater or not.")
+num1 = int(input("Enter the 1st number = "))
+num2 = int(input("Enter the 2nd number = "))
+
+print(num1 >= num2)
