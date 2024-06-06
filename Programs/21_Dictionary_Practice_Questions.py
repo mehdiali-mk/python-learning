98667601859b25aad68288cf53738b1f28e350d2
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-06-06 15:13:12 +0530

    21 - Dictionary Practice Question file is added.

---
+++ Programs/21_Dictionary_Practice_Questions.py
@@ -0,0 +1,20 @@
+"""
+Aim: Dictionary Practice Questions
+Author: Mehdiali
+Date: 31 / May / 2024 - 04:15 PM
+"""
+
+marksOfStudent = {}
+
+marksOfStudent["Maths"] =  int(input("Enter the marks of maths = "))
+marksOfStudent["Science"] = int(input("Enter the marks of science = "))
+marksOfStudent["English"] = int(input("Enter the marks of english = "))
+
+print(marksOfStudent)
+
+pyDir = {
+    "table": ["a piece of furniture", "List of facts and figures."],
+    "cat": "a small animal"
+}
+
+print(pyDir)
