ac4c85d5cd7efcae023f29af80beca9cec8d47ea
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-06-07 17:54:01 +0530

    22 to 23 - Loops Practice File is added.

---
+++ Programs/22_Set_Practice_Qestion.py
@@ -0,0 +1,16 @@
+"""
+Aim: Set Practice Questions
+Author: Mehdiali
+Date: 31 / May / 2024 - 04:15 PM
+"""
+
+mySet = {9, "9.0"}
+
+print(int(float(mySet.pop())))
+print(int(float(mySet.pop())))
+# print(int(float(mySet[1])))
+
+
+subjects = {"python", "java", "c++", "python",  "javascript", "java", "python", "java", "c++", "c"}
+
+print("There are", len(subjects), "classes required.")
---
+++ Programs/23_Loops_Practice.py
@@ -0,0 +1,23 @@
+"""
+Aim: Loops Practice Questions
+Author: Mehdiali
+Date: 31 / May / 2024 - 04:38 PM
+"""
+
+rcount, count = 100, 1
+
+while (count <= 100):
+    print(count, "\t", rcount)
+    count += 1
+    rcount -= 1
+
+myList = [1,4,9,16,25,36,49,64,81,100]
+
+count = 0
+
+while (count < len(myList)):
+    print(myList[count])
+    count += 1
+
+count = 0
+
