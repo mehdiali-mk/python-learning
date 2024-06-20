c5bb82e95d1400a5102cac37416fd7e40e978684
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-06-20 12:13:15 +0530

    DAY 1 - Folder is added.

---
+++ DAY-1/002_Swap_Two_Numbers.py
@@ -0,0 +1,15 @@
+"""
+Aim: Swap Two Numbers.
+Author: Mehdiali
+Date: 07 / June / 2024 - 03:27 PM
+"""
+
+num1 = int(input("Enter the 1st number = "))
+num2 = int(input("Enter the 2st number = "))
+
+num3 = num1
+num1 = num2
+num2 = num3
+
+print("\n1st number =", num1)
+print("2st number =", num2)
